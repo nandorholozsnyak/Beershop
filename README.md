86a07dcaa8719ce21e7f62c33279ad19a60802b5
Nándor Holozsnyák <holi60@gmail.com>
2016-02-18 11:43:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+This is going to be a project for a course called Programming Environments.
+
+TO-DO
---
+++ src/main/java/hu/hnk/progkorny/beadando/project/application/App.java
@@ -0,0 +1,13 @@
+package hu.hnk.progkorny.beadando.project.application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
