4f40aa41a17ee80482a68d07c604582c297662e8
Nándor Holozsnyák <holi60@gmail.com>
2016-03-21 22:06:29 +0100

    beershop project structure uploaded

---
+++ beershop/beershop-web/src/main/webapp/js/cufon-replace.js
@@ -0,0 +1,2 @@
+Cufon.replace('h2, #contacts-form a', { fontFamily: 'OrigGarmnd BT', hover:true });
+Cufon.replace('nav ul li a', { fontFamily: 'OrigGarmnd BT', textShadow:'1px 1px #fff', hover:true });
